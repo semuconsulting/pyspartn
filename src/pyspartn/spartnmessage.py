d88c8cbcfabe227aed34a0bf8cf9730320a6affc
semuadmin <28569967+semuadmin@users.noreply.github.com>
2023-06-30 09:27:34 +0100

    add temporary override of decode flag for OCB

--- src/pyspartn/spartnmessage.py
+++ src/pyspartn/spartnmessage.py
@@ -76,7 +76,7 @@ class SPARTNMessage:
             raise SPARTNParseError(f"Unknown message preamble {self._preamble}")
 
         self._scaling = kwargs.get("scaling", False)
-        self._decode = kwargs.get("decode", False)
+        self._decode = kwargs.get("decode", True)
         key = kwargs.get("key", getenv("MQTTKEY", None))  # 128-bit key
         self._basedate = kwargs.get(
             "basedate", datetime.now()
